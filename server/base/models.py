839ef09872f55602008d3a6a0a9234b0742d8a5a
David Jackson <david.jackson@sykes.com>
2015-07-31 17:52:36 -0500

    Updated gitignore, admin, serializers

--- server/base/models.py
+++ server/base/models.py
@@ -3,7 +3,19 @@ from django.db import models
 class Company(models.Model):
     name = models.CharField(max_length=10)
 
+    def __unicode__(self):
+        return "Company: " + self.name
+
+    class Meta:
+        verbose_name_plural = 'Companies'
+
 class StockPrice(models.Model):
     company = models.ForeignKey(Company, related_name='price_history')
     time = models.DateTimeField()
     price = models.DecimalField(max_digits=15, decimal_places=10)
+
+    def __unicode__(self):
+        return "{name}: {time}: {price}".format(
+            name=self.company.name,
+            time=self.time,
+            price=self.price)
--- server/base/serializers.py
+++ server/base/serializers.py
@@ -3,9 +3,11 @@ from rest_framework import serializers
 from . import models
 
 class CompanySerializer(serializers.ModelSerializer):
-    model = models.Company
-    fields = ['name', 'price_history']
+    class Meta:
+        model = models.Company
+        fields = ['name', 'price_history']
 
 class StockPriceSerializer(serializers.ModelSerializer):
-    model = models.StockPrice
-    fields = ['company', 'time', 'price']
+    class Meta:
+        model = models.StockPrice
+        fields = ['company', 'time', 'price']
